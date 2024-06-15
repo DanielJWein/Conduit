8df3a6ae3d7d4d74e0802a3177570d0fddec364f
DanielJWein <150991402+DanielJWein@users.noreply.github.com>
2024-06-15 18:13:35 -0400

    Fix crash if server is closed without ever listening

--- Conduit/Net/Connection/ServerListener.cs
+++ Conduit/Net/Connection/ServerListener.cs
@@ -1,16 +1,16 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+
 using Conduit.Net.Events;
 
 namespace Conduit.Net.Connection;
 
-internal class ServerListener(Socket serverSocket) : IDisposable
-{
+internal class ServerListener( Socket serverSocket ) : IDisposable {
 
     /// <summary>
     /// Represents a cancellation token source
     /// </summary>
-    protected readonly CancellationTokenSource cancellationTokenFactory = new();
+    protected readonly CancellationTokenSource cancellationTokenFactory = new( );
 
     /// <summary>
     /// Holds the listening task
@@ -25,30 +25,24 @@ internal class ServerListener(Socket serverSocket) : IDisposable
     /// <summary>
     /// Disposes this ServerListener
     /// </summary>
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
+    public void Dispose( ) {
+        Dispose( true );
+        GC.SuppressFinalize( this );
     }
 
     /// <summary>
     /// Starts listening for new connections
     /// </summary>
-    public async Task StartListening()
-    {
-        serverSocket.Listen(32);
-        try
-        {
-            listeningTask = Task.Run(() =>
-            {
-                while (!cancellationTokenFactory.IsCancellationRequested)
-                {
-                    try
-                    {
+    public async Task StartListening( ) {
+        serverSocket.Listen( 32 );
+        try {
+            listeningTask = Task.Run( ( ) => {
+                while ( !cancellationTokenFactory.IsCancellationRequested ) {
+                    try {
                         Socket st = serverSocket.Accept();
                         st.SendBufferSize = 8192;
                         IPEndPoint ipep = st.RemoteEndPoint as IPEndPoint;
-                        ClientConnected?.Invoke(this, new ClientConnectedEventArgs(ipep, st));
+                        ClientConnected?.Invoke( this, new ClientConnectedEventArgs( ipep, st ) );
                     }
                     catch { }
                 }
@@ -57,29 +51,27 @@ internal class ServerListener(Socket serverSocket) : IDisposable
 
             await listeningTask;
         }
-        catch
-        {
+        catch {
         }
     }
 
     /// <summary>
     /// Stops listening for new connections.
     /// </summary>
-    public void StopListening() => cancellationTokenFactory.Cancel();
+    public void StopListening( ) => cancellationTokenFactory.Cancel( );
 
     /// <summary>
     /// Disposes this ServerListener
     /// </summary>
     /// <param name="disposing"> If true, will dispose managed resources </param>
     /// <remarks> If <paramref name="disposing" /> is false, this function does nothing. </remarks>
-    protected async void Dispose(bool disposing)
-    {
-        if (disposing)
-        {
-            cancellationTokenFactory.Cancel();
-            await listeningTask.ConfigureAwait(false);
-            listeningTask?.Dispose();
-            cancellationTokenFactory?.Dispose();
+    protected async void Dispose( bool disposing ) {
+        if ( disposing ) {
+            cancellationTokenFactory.Cancel( );
+            if ( listeningTask is not null )
+                await listeningTask.ConfigureAwait( false );
+            listeningTask?.Dispose( );
+            cancellationTokenFactory?.Dispose( );
         }
     }
 }
