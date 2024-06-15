d3c5d6ee9fcb184c517880a17af84f52085abcf2
DanielJWein <150991402+DanielJWein@users.noreply.github.com>
2024-06-15 18:34:23 -0400

    Change empty frame detection

--- Conduit/Codec/ConduitCodecFrame.cs
+++ Conduit/Codec/ConduitCodecFrame.cs
@@ -6,15 +6,10 @@
 public sealed class ConduitCodecFrame {
 
     /// <summary>
-    /// Represents an empty frame
-    /// </summary>
-    private const string emptyFrameData = "No Frame";
-
-    /// <summary>
     /// Creates a new empty frame
     /// </summary>
     public ConduitCodecFrame( ) {
-        Data = "";
+        EncodedData = "";
     }
 
     /// <summary>
@@ -24,9 +19,9 @@ public sealed class ConduitCodecFrame {
     /// <param name="length"> The length of the input data (uses data.Length if not given) </param>
     public ConduitCodecFrame( byte[ ] data, int length = -1 ) {
         length = length == -1 ? data.Length : length;
-        SendData = new byte[ length ];
-        Array.Copy( data, SendData, length );
-        Data = Convert.ToBase64String( SendData );
+        RealData = new byte[ length ];
+        Array.Copy( data, RealData, length );
+        EncodedData = Convert.ToBase64String( RealData );
     }
 
     /// <summary>
@@ -34,45 +29,45 @@ public sealed class ConduitCodecFrame {
     /// </summary>
     /// <param name="data"> The data of this frame </param>
     public ConduitCodecFrame( string data ) {
-        Data = data;
-        SendData = Convert.FromBase64String( data );
+        EncodedData = data;
+        RealData = Convert.FromBase64String( data );
     }
 
     /// <summary>
     /// Represents an empty frame
     /// </summary>
-    public static ConduitCodecFrame EmptyFrame { get; private set; } = new( emptyFrameData );
+    public static ConduitCodecFrame EmptyFrame { get; private set; } = new( [ (byte) 0 ] );
 
     /// <summary>
     /// Represents the data stored in this frame
     /// </summary>
-    public string Data { get; private set; }
+    public string EncodedData { get; private set; }
 
     /// <summary>
     /// Returns true if the frame has no information.
     /// </summary>
-    public bool IsEmpty => Data == null || Data.Length == 0 || Data == emptyFrameData || Data.Length == 12 || Data.Length == 4;
+    public bool IsEmpty => ( EncodedData?.Length ?? 0 ) == 0 || RealDataLength <= 8;
 
     /// <summary>
     /// Represents the data stored in this frame in raw form
     /// </summary>
-    public byte[ ] SendData { get; private set; }
+    public byte[ ] RealData { get; private set; }
 
     /// <summary>
     /// Gets the length of ShortData
     /// </summary>
-    public ushort SendDataLength => (ushort) ( SendData?.Length ?? 0 );
+    public ushort RealDataLength => (ushort) ( RealData?.Length ?? 0 );
 
     /// <summary>
     /// Gets the data packet format for this frame
     /// </summary>
     /// <returns> A packet ready to be sent (includes headers and length) </returns>
     public byte[ ] GetPacket( ) {
-        byte[] outPacket = new byte[ SendData.Length + 4 ];
+        byte[] outPacket = new byte[ RealData.Length + 4 ];
         outPacket[ 0 ] = 0x01;
         outPacket[ 1 ] = 0x00;
-        Array.Copy( BitConverter.GetBytes( (ushort) SendData.Length ), 0, outPacket, 2, 2 );
-        Array.Copy( SendData, 0, outPacket, 4, SendData.Length );
+        Array.Copy( BitConverter.GetBytes( (ushort) RealData.Length ), 0, outPacket, 2, 2 );
+        Array.Copy( RealData, 0, outPacket, 4, RealData.Length );
 
         return outPacket;
     }
