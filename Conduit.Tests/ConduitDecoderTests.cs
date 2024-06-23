3a637117e666f2a25d80afc38c7d53a275e1cc98
DanielJWein <150991402+DanielJWein@users.noreply.github.com>
2024-06-23 15:33:08 -0400

    Clean up ConduitDecoderTests

--- Conduit.Tests/ConduitDecoderTests.cs
+++ Conduit.Tests/ConduitDecoderTests.cs
@@ -1,39 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Conduit.Codec;
+﻿using Conduit.Codec;
 
 namespace Conduit.Tests;
 
 public class ConduitDecoderTests {
-    private const string encData = @"/E16UiUU6VPurfhc6cizbSUXpP1TY+khblpbYlNYrCg3nLCXbhPrn9E3sXtBTB65Ilh9sKZCFi0wIzTN7zWMw++YJT8zFE677KmofvIWZeEJTwqWuykxkiThLkfn8CMV";
+    private const string EncodedData = @"/E16UiUU6VPurfhc6cizbSUXpP1TY+khblpbYlNYrCg3nLCXbhPrn9E3sXtBTB65Ilh9sKZCFi0wIzTN7zWMw++YJT8zFE677KmofvIWZeEJTwqWuykxkiThLkfn8CMV";
 
     private ConduitDecoder decoder;
 
     [Test]
     public void DecodeEmpty( ) {
+        //We need to be sure that the buffer is empty to check the result
         if ( decoder.Buffer.BufferedBytes > 0 ) {
             Assert.Fail( "The buffer was not empty." );
         }
+
+        //Actually decode an empty frame
         decoder.DecodeFrame( ConduitCodecFrame.EmptyFrame );
+
+        //We expect that zero bytes were added
         if ( decoder.Buffer.BufferedBytes > 0 ) {
             Assert.Fail( $"The DecodeFrame method added {decoder.Buffer.BufferedBytes} bytes to the buffer (expected 0)" );
         }
+
+        //We passed!
         Assert.Pass( $"{decoder.Buffer.BufferedBytes} were added to the buffer (expected !0)" );
     }
 
     [Test]
     public void DecodeReal( ) {
+        //We need to be sure that the buffer is empty to check the result.
         if ( decoder.Buffer.BufferedBytes > 0 ) {
             Assert.Fail( "The buffer was not empty." );
         }
-        decoder.DecodeFrame( new( encData ) );
+
+        //Actually decode the frame
+        decoder.DecodeFrame( new( EncodedData ) );
+
+        //We expect that more than zero bytes were added to the buffer.
         if ( decoder.Buffer.BufferedBytes <= 0 ) {
             Assert.Fail( "The DecodeFrame method did not add any bytes to the buffer." );
         }
+
+        //We passed!
         Assert.Pass( $"{decoder.Buffer.BufferedBytes} bytes were added to the buffer (expected 0)" );
     }
 
