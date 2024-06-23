f146061d55c8b084ff40a7baeda33c777ca0fc20
DanielJWein <150991402+DanielJWein@users.noreply.github.com>
2024-06-23 16:12:11 -0400

    Add alerter tests

---
+++ Conduit.Tests/AlerterTests.cs
@@ -0,0 +1,30 @@
+﻿using Conduit.Util;
+
+namespace Conduit.Tests;
+
+public class AlerterTests {
+
+    [Test]
+    public void IntAlerter( ) {
+        bool eventRaised = false;
+
+        Alerter<int> alerter = new(0);
+        alerter.ValueChanged += ( object? o, EventArgs e ) => eventRaised = true;
+
+        alerter.Value = 1;
+
+        if ( !eventRaised ) {
+            Assert.Fail( "The event was not raised." );
+        }
+
+        int x = 0;
+
+        x = alerter;
+
+        if ( x != 1 ) {
+            Assert.Fail( "The value was not converted correctly." );
+        }
+
+        Assert.Pass( "The event was raised." );
+    }
+}
